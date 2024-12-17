597014d19ec4fbf7f7979d4ef2757e409f5310eb
CHERIBET CHERIF CHOUAIB <33702774+chouaibcher@users.noreply.github.com>
2024-12-17 14:02:11 +0100

    Update MyFunctions.py

--- MyFunctions.py
+++ MyFunctions.py
@@ -8,7 +8,6 @@ def saveOutput(fun,y):
             file.write("----------------------------\n")
             file.write(f"{fun}\n")
             file.write(f"y'={y}\n\n\n")
-            file.write("Neural networks are not just for solving complex problems;\n they are also tools for gaining a deeper understanding of patterns and relationships within data.\n - Sebastian Thrun\n")
     else:
         print("Goodbye ...")
         exit()
