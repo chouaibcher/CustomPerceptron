87c929c9c9ea3a81354bfc477c4434face13e474
CHERIBET CHERIF CHOUAIB <33702774+chouaibcher@users.noreply.github.com>
2024-12-17 23:41:45 +0100

    Update MyFunctions.py

--- MyFunctions.py
+++ MyFunctions.py
@@ -3,9 +3,7 @@ def saveOutput(fun,y):
     save=input("Do you want to save the output? (yes/no):")
     if str(save).lower()=='yes':
         print('Done')
-        with open("output.txt","w") as file:
-            file.write("--CHERIBET CHERIF Chouaib--\n")
-            file.write("----------------------------\n")
+        with open("output.txt","w") as file:                  file.write("----------------------------\n")
             file.write(f"{fun}\n")
             file.write(f"y'={y}\n\n\n")
     else:
